d3647ba85ac2d26ecf79336fe76cd0fed23f4959
shivankartft <bansal.shivankar@tftus.com>
2024-10-18 19:20:57 +0530

    refactor code

--- src/tests/search.spec.ts
+++ src/tests/search.spec.ts
@@ -1,14 +1,14 @@
-// import { test } from "../fixtures/fixture";
+import { test } from "../fixtures/fixture";
 
-// test.describe("Search for a product and select last third result", () => {
-//   test(`Search for a product and select last third result`, async ({ page, logger }) => {
-//     await page.productsPage.navigateToProductsPage();
-//     logger.info("Successfully navigated to products page");
+test.describe("Search for a product and select last third result", () => {
+  test(`Search for a product and select last third result`, async ({ page, logger }) => {
+    await page.productsPage.navigateToProductsPage();
+    logger.info("Successfully navigated to products page");
 
-//     await page.productsPage.searchForAItem();
-//     logger.info("Successfully search for shirt");
+    await page.productsPage.searchForAItem();
+    logger.info("Successfully search for shirt");
 
-//     await page.productsPage.selectLastThirdSearchResult();
-//     logger.info("Successfully added the third item to cart");
-//   });
-// });
+    await page.productsPage.selectLastThirdSearchResult();
+    logger.info("Successfully added the third item to cart");
+  });
+});
